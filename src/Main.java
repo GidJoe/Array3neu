66ca04c451f3ff0a2a3bc300e6e0ad6ee6ab6e41
GidJoe <winterm@bfwob.de>
2023-01-10 15:21:10 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String[] firmen = {"Opel","VW","Mercedes","Citroen","Audi"};
-        double[] umsatz = {2522057.87,622097.22,1709103.01,145089.97,1004472.77};
+        double[] umsatz = {522_057.87,922_097.22,1_709_103.01,145_089.97,1_004_472.77};
 
         double max = 0;
         int maxIndex = 0;
@@ -12,5 +12,6 @@ public class Main {
             }
         }
         System.out.println(firmen[maxIndex]);
+        System.out.println(umsatz[maxIndex]+" €");
     }
 }
